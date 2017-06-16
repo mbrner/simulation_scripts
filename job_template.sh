0509d1394fd7b93967d1ea07da0f7e0e72cde772
Mathis Börner <mathis.boerner@tu-dortmund.de>
2017-06-16 17:31:15 -0600

    debugging job_template.sh

--- job_template.sh
+++ job_template.sh
@@ -5,9 +5,9 @@
 #PBS -o {job_file_folder}/logs/step_{step_number}_run_{run_number}_${PBS_JOBID}.out
 #PBS -e {job_file_folder}/logs/step_{step_number}_run_{run_number}_${PBS_JOBID}.err
 #PBS -q long
-FINAL_OUT = {final_out}
+FINAL_OUT={final_out}
 echo $FINAL_OUT
-if [-z ${PBS_JOBID}] || [-z ${CLUSTER}]
+if [ -z ${PBS_JOBID} ] || [ -z ${CLUSTER} ]
 then
     {script_folder}/step_{step_number}.py {yaml_copy} -r {run_number} --scratch
     cp {scratch_out} $FINAL_OUT
