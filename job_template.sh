8a0291c473a8fbf27c298cc97dba675d80b24ab5
Mathis Börner <mathis.boerner@tu-dortmund.de>
2017-06-16 17:08:31 -0600

    removed unneeded options

--- job_template.sh
+++ job_template.sh
@@ -6,5 +6,6 @@
 #PBS -e {job_file_folder}/logs/step_{step_number}_run_{run_number}_${PBS_JOBID}.err
 #PBS -q long
 FINAL_OUT = {final_out}
+echo $FINAL_OUT
 {script_folder}/step_{step_number}.py {yaml_copy} -r {run_number}
-cp {scratch_out} {final_out}
+cp {scratch_out} $FINAL_OUT
