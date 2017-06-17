e5bb489f34d001e3151a28bb30a961066560ef10
Mathis Börner <mathis.boerner@tu-dortmund.de>
2017-06-16 21:22:44 -0500

    removed falsly added cp for cluster run

--- job_template.sh
+++ job_template.sh
@@ -12,16 +12,18 @@ if [ -z ${PBS_JOBID} ] && [ -z ${CLUSTER} ]
 then
     echo 'Running Script w/o temporary scratch'
     {script_folder}/step_{step_number}.py {yaml_copy} {run_number} --no-scratch
+    echo 'IceTray finished with Exit Code: ' $?
     if [ "$?" != "0" ] ; then
-        echo 'IceTray finished with Exit Code: $?'
+        rm $FINAL_OUT
     fi
 else
     echo 'Running Script w/ temporary scratch'
     {script_folder}/step_{step_number}.py {yaml_copy} {run_number} --scratch
+    echo 'IceTray finished with Exit Code: ' $?
     if [ "$?" = "0" ] ; then
         cp {scratch_out} $FINAL_OUT
-    else:
-        echo 'IceTray finished with Exit Code: $?'
+    else
+        rm {scratch_out}
     fi
 fi
 
