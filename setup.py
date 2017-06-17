fdfdc340354afa108f80f8dba1bb0ce291d2d480
Mathis Börner <mathis.boerner@tu-dortmund.de>
2017-06-17 13:35:18 -0500

    added local processing as entry point

--- setup.py
+++ setup.py
@@ -11,5 +11,6 @@ setup(
     entry_points='''
         [console_scripts]
         simulation_scripts_write=simulation_scripts:main
+        simulation_scripts_process=batch_processing:process_local
     ''',
 )
