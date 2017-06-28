7cf85adfc2a29758304a71f93cb0e79566815a53
Mathis Börner <mathis.boerner@tu-dortmund.de>
2017-06-27 20:55:37 -0500

    added resume and graceful exit to process_local

--- process_local.py
+++ process_local.py
@@ -1,6 +1,9 @@
 import os
 import subprocess
 import glob
+import signal
+import sys
+import copy
 
 import click
 
@@ -12,15 +15,21 @@ class JobLogBook(object):
         self.n_jobs = n_jobs
         self.n_running = 0
         self.n_finished = 0
+        self.log = []
+        self._original_sigint = None
 
     def process(self, binaries):
+        self.__binaries = copy.copy(binaries)
         with click.progressbar(length=len(binaries)) as bar:
-            for job in binaries:
+            bar.update(self.n_finished)
+            for i, job in enumerate(binaries):
                 if os.path.isfile(job) and os.access(job, os.X_OK):
                     self.__start_subprocess__(job)
                 else:
                     click.echo('{} is not executable! (Skipped)'.format(job))
                     self.n_finished += 1
+                    self.log.append([job, 'not_executable'])
+                    del self.__binaries[i]
                 if self.n_running >= self.n_jobs:
                     self.__wait__(bar)
             click.echo('All Jobs started. Wait for last jobs to finish!')
@@ -28,15 +37,59 @@ class JobLogBook(object):
                 self.__wait__(bar)
             bar.update(self.n_finished)
         click.echo('Finished!')
+        if self.log_dir is not None:
+            self.__store__()
 
-    def __wait__(self, progressbar):
+    def __wait__(self, progressbar=None):
         pid, exit_code = os.wait()
         job_file = self.logbook[pid][1]
         click.echo('{} finished with exit code {}'.format(
             job_file, exit_code))
+        self.log.append([job_file, exit_code])
         self.n_finished += 1
         self.__clear_job__(pid)
-        progressbar.update(1)
+        if progressbar is not None:
+            progressbar.update(1)
+
+    def __wait_rest__(self, save=False):
+        click.echo('waiting for all subprocesses to finish')
+        while self.n_running > 0:
+            self.__wait__()
+        if save:
+            self.__store__()
+
+    def __store__(self):
+        path = os.path.join(self.log_dir, 'resume.txt')
+        binary_set = set(self.__binaries)
+        finished_jobs = set([l[0] for l in self.log])
+        unfinished_jobs = binary_set.difference(finished_jobs)
+        with open(path, 'w') as f:
+            for job, exit_code in self.log:
+                f.write("{};{}\n".format(job, exit_code))
+            for job in unfinished_jobs:
+                f.write("{};\n".format(job))
+
+    def resume(self, resume_file):
+        with open(resume_file) as f:
+            content = f.readlines()
+        content = [c.strip() for c in content]
+        retry = click.confirm('Retry failed jobs?')
+        binaries = []
+        for c in content:
+            try:
+                job, exit_code = c.split(';')
+            except ValueError:
+                raise ValueError('{} can be resumed!'.format(resume_file))
+            if retry:
+                if exit_code != '0':
+                    binaries.append(job)
+            else:
+                if exit_code == '':
+                    binaries.append(job)
+        if len(binaries) > 0:
+            self.process(binaries)
+        else:
+            click.echo('Nothing to do!')
 
     def __start_subprocess__(self, job):
         job_name = os.path.basename(os.path.splitext(job)[0])
@@ -61,6 +114,26 @@ class JobLogBook(object):
             log_file.close()
         del self.logbook[pid]
 
+    def register_sigint(self):
+        if self._original_sigint is None:
+            self._original_sigint = signal.getsignal(signal.SIGINT)
+
+        def exit_gracefully(signum, frame):
+            signal.signal(signal.SIGINT, self._original_sigint)
+            try:
+                if click.confirm('Really want to quit?'):
+                    if self.log_dir is not None:
+                        save = click.confirm('Store progress?')
+                    else:
+                        save = False
+                    self.__wait_rest__(save)
+            except KeyboardInterrupt:
+                click.echo("Ok ok, quitting")
+                sys.exit(1)
+            self.register_sigint()
+
+        signal.signal(signal.SIGINT, exit_gracefully)
+
 
 @click.command()
 @click.argument('path', click.Path(exists=True, resolve_path=True))
@@ -71,14 +144,23 @@ class JobLogBook(object):
 @click.option('-l', '--log_path', default=None,
               type=click.Path(resolve_path=True),
               help='Path to a dir where the stdout/stderr should be saved')
-def main(path, binary_pattern, n_jobs, log_path):
+@click.options('--resume/--no-resume')
+def main(path, binary_pattern, n_jobs, log_path, resume):
     path = os.path.abspath(path)
-    binaries = list(glob.glob(os.path.join(path, binary_pattern)))
-    click.echo('Processing {} with max. {} parralel jobs!'.format(
-        len(binaries), n_jobs))
-    click.echo('Starting processing!')
+
     log_book = JobLogBook(n_jobs=n_jobs, log_dir=log_path)
+    log_book.register_sigint()
+    click.echo('Starting processing!')
+    if resume:
+        click.echo('Resuming {} with max. {} parralel jobs!'.format(
+            path, n_jobs))
+        log_book.resume(path)
+    else:
+        binaries = list(glob.glob(os.path.join(path, binary_pattern)))
+        click.echo('Processing {} with max. {} parralel jobs!'.format(
+            len(binaries), n_jobs))
     log_book.process(binaries)
+
 
 if __name__ == '__main__':
     main()
