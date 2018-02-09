6b1e867dd3f665457f98e80e6ed8a49f474fcb0d
Maximilian Meier <maximilian.meier@tu-dortmund.de>
2018-02-09 17:28:53 +0100

    Shower oversampling step 0

---
+++ steps/step_0_shower_oversampling.py
@@ -0,0 +1,134 @@
+#!/bin/sh /cvmfs/icecube.opensciencegrid.org/py2-v2/icetray-start
+#METAPROJECT /home/mmeier/combo_test/build
+from __future__ import division
+
+import os
+
+from icecube import VHESelfVeto
+
+
+class MCTreeStripper(icetray.I3ConditionalModule):
+    def __init__(self, context):
+        icetray.I3ConditionalModule.__init__(self, context)
+
+    def Configure(self):
+        pass
+
+    def DAQ(self, frame):
+        mctree = frame['I3MCTree']
+        new_tree = dataclasses.I3MCTree()
+
+        primaries = mctree.get_primaries()
+        for primary in primaries:
+            new_tree.add_primary(primary)
+            daughters = mctree.get_daughters(primary)
+            new_tree.append_children(primary, daughters)
+
+        del frame['I3MCTree']
+        frame['I3MCTree'] = new_tree
+
+        self.PushFrame(frame)
+
+    def Physics(self, frame):
+        pass
+
+
+class QFactory(icetray.I3ConditionalModule):
+    def __init__(self, context):
+        icetray.I3ConditionalModule.__init__(self, context)
+        self.AddParameter('n_events_per_event', '', 10)
+
+    def Configure(self):
+        self.n_events = self.GetParameter('n_events_per_event')
+
+    def DAQ(self, frame):
+        for i in range(self.n_events):
+            frame['CorsikaWeightMap']['Oversampling'] = float(self.n_events)
+            self.PushFrame(frame)
+
+
+@click.command()
+@click.argument('cfg', click.Path(exists=True))
+@click.argument('run_number', type=int)
+@click.option('--scratch/--no-scratch', default=True)
+def main(cfg, run_number, scratch):
+    with open(cfg, 'r') as stream:
+        cfg = yaml.load(stream)
+    cfg['run_number'] = run_number
+    cfg['run_folder'] = get_run_folder(run_number)
+    if scratch:
+        outfile = cfg['scratchfile_pattern'].format(**cfg)
+    else:
+        outfile = cfg['outfile_pattern'].format(**cfg)
+    outfile = outfile.replace(' ', '0')
+
+    infile = cfg['infile_pattern'].format(**cfg)
+    infile = infile.replace(' ', '0')
+
+    if not os.path.isfile(infile):
+        exit('File does not exist! Skipping it...')
+
+    click.echo('Run: {}'.format(run_number))
+    click.echo('Outfile: {}'.format(outfile))
+    click.echo('n_events_per_event: {}'.format(cfg['n_events_per_event']))
+
+    tray = I3Tray()
+    random_services, _ = create_random_services(
+        dataset_number=cfg['dataset_number'],
+        run_number=cfg['run_number'],
+        seed=cfg['seed'],
+        n_services=2)
+
+    # Read an input file
+    tray.AddModule('I3Reader', 'read l2 corsika file',
+                   Filenamelist=[cfg['gcd'], infile])
+
+    # Apply a Qtot cut
+    tray.AddModule('HomogenizedQTot', 'Qtot calculation',
+                   Pulses=cfg['pulses'])
+
+    tray.AddModule('VHESelfVeto', 'selfveto',
+                   TimeWindow=3000,
+                   VertexThreshold=250,
+                   DustLayer=-160,
+                   DustLayerWidth=60,
+                   VetoThreshold=3,
+                   Pulses=cfg['pulses'])
+
+    tray.AddModule('HomogenizedQTot', 'Causal Qtot',
+                   Pulses=cfg['pulses'],
+                   Qutput='CausalQTot',
+                   VertexTime='VHESelfVetoVertexTime')
+
+    tray.AddModule(lamda frame: np.log10(frame['CausalQTot'].value) >= 3.)
+
+    # Strip down the MCTree and inject n_events_per_event new Q Frames
+    tray.AddModule(MCTreeStripper, 'Strip down the MCTree')
+
+    keep_before_resim = [
+        'I3MCTree',
+        'CorsikaWeightMap'
+    ]
+
+    tray.AddModule("Keep", "keep_before_merge",
+                   keys=keep_before_resim)
+
+    tray.AddModule(QFactory, 'produce some q frames',
+                   n_events_per_event=cfg['n_events_per_event'])
+
+    # Propagate Muons
+    tray.AddSegment(segments.PropagateMuons,
+                    'propagate_muons',
+                    RandomService=random_services[1],
+                    InputMCTreeName='I3MCTree')
+
+    tray.AddModule('I3Writer', 'write',
+                   Filename=outfile,
+                   Streams=[icetray.I3Frame.DAQ, icetray.I3Frame.Stream('M')])
+    tray.AddModule('TrashCan', 'trash')
+    tray.Execute(10)
+    tray.Finish()
+    del tray
+
+if __name__ == '__main__':
+    main()
