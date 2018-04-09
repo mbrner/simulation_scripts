1aefcee97383d4b326430ed76b9c5dae4023544b
Mathis Börner <mathis.boerner@tu-dortmund.de>
2018-04-09 11:22:24 +0200

    added chain to process nancy nugen level2 to muonL3

---
+++ job_templates/nancy_mc_2012_muon_L3.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn={cpus}
+#PBS -l pmem={memory}
+#PBS -l mem={memory}
+#PBS -l vmem={memory}
+#PBS -l pvmem={memory}
+#PBS -l walltime={walltime}
+#PBS -o {processing_folder}/logs/{step_name}_run_{run_number}_${PBS_JOBID}.out
+#PBS -e {processing_folder}/logs/{step_name}_run_{run_number}_${PBS_JOBID}.err
+#PBS -q long
+#PBS -S /cvmfs/icecube.opensciencegrid.org/py2-v2/icetray-start
+FINAL_OUT={final_out}
+KEEP_CRASHED_FILES={keep_crashed_files}
+
+echo 'Loading py2-v1'
+eval `/cvmfs/icecube.opensciencegrid.org/py2-v1/setup.sh`
+export PYTHONUSERBASE=/home/mboerner/software/python_libs_py2_v1
+
+export PATH=$PYTHONUSERBASE/bin:$PATH
+export PYTHONPATH=$PYTHONUSERBASE/lib/python2.7/site-packages:$PYTHONPATH
+export PATH=$PATH:/home/mboerner/software/zstd/src
+
+echo $FINAL_OUT
+if [ -z ${PBS_JOBID} ] && [ -z ${_CONDOR_SCRATCH_DIR} ]
+then
+    echo 'Running Script w/o temporary scratch'
+    zstd -d -c /data/ana/Cscd/StartingEvents/NuGen_new/NuMu/medium_energy/IC86_flasher_p1\=0.3_p2\=0.0/l2/1/l2_00000999.i3.zst > test.i3
+    {script_folder}/steps/{step_name}.py {yaml_copy} {run_number} --no-scratch
+    ICETRAY_RC=$?
+    echo 'IceTray finished with Exit Code: ' $ICETRAY_RC
+    if [ $ICETRAY_RC -ne 0 ] && [ $KEEP_CRASHED_FILES -eq 0 ] ; then
+        echo 'Deleting partially processed file! ' $FINAL_OUT
+        rm $FINAL_OUT
+    fi
+else
+    echo 'Running Script w/ temporary scratch'
+    if [ -z ${_CONDOR_SCRATCH_DIR} ]
+    then
+        cd /scratch/mboerner
+    else
+        cd ${_CONDOR_SCRATCH_DIR}
+    fi
+    {script_folder}/steps/{step_name}.py {yaml_copy} {run_number} --scratch
+    ICETRAY_RC=$?
+    echo 'IceTray finished with Exit Code: ' $ICETRAY_RC
+    if [ $ICETRAY_RC -eq 0 ] || [ $KEEP_CRASHED_FILES -eq 1 ]; then
+        cp *.i3.bz2 {output_folder}
+    fi
+    rm *.i3.bz2
+fi
+exit $ICETRAY_RC
+
---
+++ steps/step_0_2012_muon_L3_nancy_mc.py
@@ -0,0 +1,92 @@
+#!/bin/sh /cvmfs/icecube.opensciencegrid.org/py2-v1/icetray-start
+#METAPROJECT /home/mboerner/software/i3/IC2012-L3_Muon_V3/build
+import os
+
+import click
+import yaml
+import subprocess
+
+from utils import get_run_folder
+
+from I3Tray import I3Tray
+from icecube import icetray, dataio, dataclasses, hdfwriter, phys_services
+from icecube import lilliput, gulliver, gulliver_modules
+from icecube import improvedLinefit, rootwriter
+
+from icecube.level3_filter_muon.MuonL3TraySegment import MuonL3
+
+SPLINE_TABLES = '/cvmfs/icecube.opensciencegrid.org/data/photon-tables/splines'
+PHOTON_TABLES = '/cvmfs/icecube.opensciencegrid.org/data/photon-tables'
+DRIVER_FILE = 'mu_photorec.list'
+
+
+@click.command()
+@click.argument('cfg', click.Path(exists=True))
+@click.argument('run_number', type=int)
+@click.option('--scratch/--no-scratch', default=True)
+def main(cfg, run_number, scratch):
+    cfg['variation_name'] = cfg['variations'][cfg['step'] // 10]
+    with open(cfg, 'r') as stream:
+        cfg = yaml.load(stream)
+    icetray.logging.set_level("WARN")
+    cfg['run_number'] = run_number
+    cfg['run_folder'] = (run_number % 1000) + 1
+    infile = cfg['infile_pattern'].format(**cfg)
+    infile = infile.replace(' ', '0')
+    tmpfile = os.path.join(cfg['processing_scratch'],
+                            'tmp_{}.i3'.format(run_number))
+
+    cmd = 'zstd -d -c {} > {}'.format(infile, tmpfile)
+    subprocess.Popen(cmd,
+                     shell=True,
+                     stdout=subprocess.PIPE).stdout.read()
+    if scratch:
+        outfile = cfg['scratchfile_pattern'].format(**cfg)
+    else:
+        outfile = cfg['outfile_pattern'].format(**cfg)
+    outfile = os.path.dirname(outfile)
+    outfile = os.path.join(
+        outfile,
+        'Level3_IC86.2012_nugen_numu.{dataset_number:6d}.{run_number:6d}.{variation_name:}.i3.bz2'.format(cfg['dataset_name'],
+                                            run_number,
+                                            cfg['variation_name']))
+    outfile = outfile.replace(' ', '0')
+    print('Outfile != $FINAL_OUT clean up for crashed scripts not possible!')
+    print(infile)
+    print(tmpfile)
+    print(outfile)
+    exit()
+
+
+    outfile = outfile.replace(' ', '0')
+
+    tray = I3Tray()
+
+    photonics_dir = os.path.join(PHOTON_TABLES, 'SPICEMie')
+    photonics_driver_dir = os.path.join(photonics_dir, 'driverfiles')
+
+    tray.AddSegment(
+        MuonL3,
+        gcdfile=cfg['gcd'],
+        infiles=tmpfile,
+        output_i3=outfile,
+        output_hd5=None,
+        output_root=None,
+        photonicsdir=photonics_dir,
+        photonicsdriverdir=photonics_driver_dir,
+        photonicsdriverfile=DRIVER_FILE,
+        infmuonampsplinepath=os.path.join(SPLINE_TABLES,
+                                          'InfBareMu_mie_abs_z20a10.fits'),
+        infmuonprobsplinepath=os.path.join(SPLINE_TABLES,
+                                           'InfBareMu_mie_prob_z20a10.fits'),
+        cascadeampsplinepath=os.path.join(SPLINE_TABLES,
+                                          'ems_mie_z20_a10.abs.fits'),
+        cascadeprobsplinepath=os.path.join(SPLINE_TABLES,
+                                           'ems_mie_z20_a10.prob.fits'))
+
+    tray.AddModule("TrashCan", "Bye")
+    tray.Execute()
+    tray.Finish()
+
+if __name__ == '__main__':
+    main()
