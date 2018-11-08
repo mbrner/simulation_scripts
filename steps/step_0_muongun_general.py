8241a2cb177b70c66e5440c0ebe9ba46adc44aa6
mhuen <mhuen@users.noreply.github.com>
2018-11-08 15:23:22 +0100

    Changed e_min and e_max units from TeV to GeV to be consistent with comments in default config as well as other simulation steps; Warning: this will produce different results for old configs!

--- steps/step_0_muongun_general.py
+++ steps/step_0_muongun_general.py
@@ -37,42 +37,43 @@ def main(cfg, run_number, scratch):
     model = MuonGun.load_model(cfg['muongun_model'])
     model.flux.min_multiplicity = cfg['muongun_min_multiplicity']
     model.flux.max_multiplicity = cfg['muongun_max_multiplicity']
-    spectrum = MuonGun.OffsetPowerLaw(  cfg['gamma'],
-                                        cfg['e_min']*icetray.I3Units.TeV,
-                                        cfg['e_min']*icetray.I3Units.TeV,
-                                        cfg['e_max']*icetray.I3Units.TeV)
+    spectrum = MuonGun.OffsetPowerLaw(cfg['gamma'],
+                                      cfg['e_min']*icetray.I3Units.GeV,
+                                      cfg['e_min']*icetray.I3Units.Gev,
+                                      cfg['e_max']*icetray.I3Units.GeV)
     surface = MuonGun.Cylinder(1600, 800,
-                            dataclasses.I3Position(31.25, 19.64, 0))
+                               dataclasses.I3Position(31.25, 19.64, 0))
 
     if cfg['muongun_generator'] == 'energy':
         scale = MuonGun.BasicSurfaceScalingFunction()
         scale.SetSideScaling(4., 17266, 3.41, 1.74)
         scale.SetCapScaling(4., 23710, 3.40, 1.88)
         generator = MuonGun.EnergyDependentSurfaceInjector(surface,
-                                                            model.flux,
-                                                            spectrum,
-                                                            model.radius,
-                                                            scale)
+                                                           model.flux,
+                                                           spectrum,
+                                                           model.radius,
+                                                           scale)
     elif cfg['muongun_generator'] == 'static':
         generator = MuonGun.StaticSurfaceInjector(surface,
-                                                    model.flux,
-                                                    spectrum,
-                                                    model.radius)
-    elif cfg['muongun_generator'] =='floodlight':
-        generator = MuonGun.Floodlight(surface = surface,
-                                       energyGenerator=spectrum,
-                                       cosMin=cfg['muongun_floodlight_min_cos'],
-                                       cosMax=cfg['muongun_floodlight_max_cos'],
-                                       )
+                                                  model.flux,
+                                                  spectrum,
+                                                  model.radius)
+    elif cfg['muongun_generator'] == 'floodlight':
+        generator = MuonGun.Floodlight(
+                                surface=surface,
+                                energyGenerator=spectrum,
+                                cosMin=cfg['muongun_floodlight_min_cos'],
+                                cosMax=cfg['muongun_floodlight_max_cos'],
+                                )
     else:
         err_msg = 'MuonGun generator {} is not known.'
         err_msg += " Must be 'energy','static' or 'floodlight"
         raise ValueError(err_msg.format(cfg['muongun_generator']))
 
     tray.Add(MuonGun.segments.GenerateBundles, 'MuonGenerator',
-                            Generator=generator,
-                            NEvents=cfg['n_events_per_run'],
-                            GCDFile=cfg['gcd'])
+             Generator=generator,
+             NEvents=cfg['n_events_per_run'],
+             GCDFile=cfg['gcd'])
 
     tray.Add("Rename", keys=["I3MCTree", "I3MCTree_preMuonProp"])
 
