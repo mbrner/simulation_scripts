116d4f851e81dfbf087986c5406d2e45276ac9f8
mhuen <mhuen@users.noreply.github.com>
2019-12-05 13:05:07 +0100

    use cvmfs metaproject

--- steps/step_2_pass2_detector_simulation_py2-v3.0.1.py
+++ steps/step_2_pass2_detector_simulation_py2-v3.0.1.py
@@ -1,6 +1,6 @@
 #!/bin/sh /cvmfs/icecube.opensciencegrid.org/py2-v3.0.1/icetray-start
 #METAPROJECT simulation/V06-01-02
-from step_1_general_photon_propagation import *
+from step_2_pass2_detector_simulation import *
 
 if __name__ == '__main__':
     main()
