7ce837c5d6739eabfcf28ce26792856be429b830
mhuen <mhuen@users.noreply.github.com>
2020-06-22 11:34:12 +0200

    add templates for py3-v4.1.0

---
+++ steps/step_3_pass2_L1_py3_v4_1_0.py
@@ -0,0 +1,6 @@
+#!/bin/sh /cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/icetray-start
+#METAPROJECT /mnt/lfs7/user/mhuennefeld/software/icecube/py3-v4.1.0/combo_V01-00-00-RC0/build
+from step_3_pass2_L1 import *
+
+if __name__ == '__main__':
+    main()
