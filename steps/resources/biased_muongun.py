c69d38d573d238c441e9b7b25568daeb382d4827
mhuen <mhuen@users.noreply.github.com>
2020-08-10 11:23:39 +0200

    Biased MuonGun

---
+++ steps/resources/biased_muongun.py
@@ -0,0 +1,189 @@
+from __future__ import division
+
+from I3Tray import I3Tray
+from icecube import icetray, dataclasses, dataio
+
+from ic3_labels.labels.utils import detector
+from ic3_labels.labels.utils import muon as mu_utils
+
+
+class MuonGeometryFilter(icetray.I3ConditionalModule):
+
+    """Class to filter out muons based on their geometry.
+    This is intended to run before PROPOSAL.
+
+    Attributes
+    ----------
+    mctree_name : str
+        The name of the I3MCTree key.
+    range_length_inside_combined : None or tuple of float
+        Allowed range for combined length in upper and lower
+        convex hulls.
+    range_length_inside_icecube : None or tuple of float
+        Allowed range for length in IceCube hull.
+    range_length_inside_icecube_lower : None or tuple of float
+        Allowed range for length in lower IceCube hull.
+    range_length_inside_icecube_upper : None or tuple of float
+        Allowed range for length in upper IceCube hull.
+    """
+
+    def __init__(self, context):
+        """Class to import events from another I3-File
+
+        Parameters
+        ----------
+        context : TYPE
+            Description
+        """
+        icetray.I3ConditionalModule.__init__(self, context)
+        self.AddOutBox('OutBox')
+        self.AddParameter(
+            'range_length_inside_combined',
+            'Only muons with a combined length inside the upper and lower '
+            'IceCube hulls within this specified range are selected. '
+            'If None no selection will be performed based on this variable.',
+            None)
+        self.AddParameter(
+            'range_length_inside_icecube',
+            'Only muons with a length inside the IceCube hull within this '
+            'specified range are selected. '
+            'If None no selection will be performed based on this variable.',
+            None)
+        self.AddParameter(
+            'range_length_inside_icecube_upper',
+            'Only muons with a length inside the upper IceCube hull within '
+            'this specified range are selected. '
+            'If None no selection will be performed based on this variable.',
+            None)
+        self.AddParameter(
+            'range_length_inside_icecube_lower',
+            'Only muons with a length inside the lower IceCube hull within '
+            'this specified range are selected. '
+            'If None no selection will be performed based on this variable.',
+            None)
+        self.AddParameter(
+            'mctree_name', 'Name of I3MCTree.' 'I3MCTree_preMuonProp')
+
+    def Configure(self):
+        """Configures MuonGeometryFilter.
+        """
+        self.range_length_inside_combined = self.GetParameter(
+            'range_length_inside_combined')
+        self.range_length_inside_icecube = self.GetParameter(
+            'range_length_inside_icecube')
+        self.range_length_inside_icecube_upper = self.GetParameter(
+            'range_length_inside_icecube_upper')
+        self.range_length_inside_icecube_lower = self.GetParameter(
+            'range_length_inside_icecube_lower')
+
+        # sanity checks
+        for value_range in (
+                self.range_length_inside_combined,
+                self.range_length_inside_icecube,
+                self.range_length_inside_icecube_upper,
+                self.range_length_inside_icecube_lower,
+                ):
+            if value_range is not None:
+                assert len(value_range) == 2, value_range
+                assert value_range[1] > value_range[0], value_range
+
+        self.mctree_name = self.GetParameter(mctree_name)
+
+    def DAQ(self, frame):
+        """Filter events based on muon geometry.
+        """
+
+        # get primary
+        mc_tree = frame[mctree_name]
+        primaries = mc_tree.get_primaries()
+        assert len(primaries) == 1, 'Expected only 1 Primary!'
+
+        # get muon
+        muon = mu_utils.get_muon(frame, primaries[0], detector.icecube_hull)
+
+        passed_filter = True
+
+        # ------------------
+        # Filter for lengths
+        # ------------------
+        dist_icecube = mu_utils.get_muon_track_length_inside(
+                muon, detector.icecube_hull)
+        dist_upper = mu_utils.get_muon_track_length_inside(
+                muon, detector.icecube_hull_upper)
+        dist_lower = mu_utils.get_muon_track_length_inside(
+                muon, detector.icecube_hull_lower)
+
+        value_list = [
+            dist_icecube,
+            dist_upper,
+            dist_lower,
+            dist_lower + dist_upper,
+        ]
+        range_list = [
+            self.range_length_inside_icecube,
+            self.range_length_inside_icecube_upper,
+            self.range_length_inside_icecube_lower,
+            self.range_length_inside_combined,
+        ]
+
+        for value, allowed_range in zip(value_list, range_list):
+            if range_list is not None:
+                if value < allowed_range[0] or value > allowed_range[1]:
+                    passed_filter = False
+        # ------------------
+
+        if passed_filter:
+            self.PushFrame(frame)
+
+
+class MuonLossProfileFilter(icetray.I3ConditionalModule):
+
+    """Class to filter out muons based on their loss profile.
+    This is intended to run after PROPOSAL.
+
+    Attributes
+    ----------
+    mctree_name : str
+        The name of the I3MCTree key.
+    """
+
+    def __init__(self, context):
+        """Class to import events from another I3-File
+
+        Parameters
+        ----------
+        context : TYPE
+            Description
+        """
+        icetray.I3ConditionalModule.__init__(self, context)
+        self.AddOutBox('OutBox')
+        self.AddParameter(
+            'mctree_name', 'Name of I3MCTree.' 'I3MCTree_preMuonProp')
+
+    def Configure(self):
+        """Configures MuonLossProfileFilter.
+        """
+        self.mctree_name = self.GetParameter(mctree_name)
+
+    def DAQ(self, frame):
+        """Filter events based on muon geometry.
+        """
+
+        # get primary
+        mc_tree = frame[mctree_name]
+        primaries = mc_tree.get_primaries()
+        assert len(primaries) == 1, 'Expected only 1 Primary!'
+
+        # get muon
+        muon = mu_utils.get_muon(frame, primaries[0], detector.icecube_hull)
+
+        passed_filter = True
+
+        # -------------
+        # filter events
+        # -------------
+        # ToDo: add filtering logic
+        # -------------
+
+        if passed_filter:
+            self.PushFrame(frame)
---
+++ steps/step_0_biased_muongun.py
@@ -0,0 +1,169 @@
+#!/bin/sh /cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/icetray-start
+#METAPROJECT /mnt/lfs7/user/mhuennefeld/software/icecube/py3-v4.1.0/combo_V01-00-00/build
+from __future__ import division
+import click
+import yaml
+import numpy as np
+
+from icecube.simprod import segments
+
+from I3Tray import I3Tray, I3Units
+from icecube import icetray, dataclasses
+
+from utils import create_random_services, get_run_folder
+from resources.oversampling import DAQFrameMultiplier
+from resources.biased_muongun import MuonGeometryFilter
+from resources.biased_muongun import MuonLossProfileFilter
+
+
+@click.command()
+@click.argument('cfg', type=click.Path(exists=True))
+@click.argument('run_number', type=int)
+@click.option('--scratch/--no-scratch', default=True)
+def main(cfg, run_number, scratch):
+    with open(cfg, 'r') as stream:
+        cfg = yaml.full_load(stream)
+    cfg['run_number'] = run_number
+    cfg['run_folder'] = get_run_folder(run_number)
+    if scratch:
+        outfile = cfg['scratchfile_pattern'].format(**cfg)
+    else:
+        outfile = cfg['outfile_pattern'].format(**cfg)
+    outfile = outfile.replace(' ', '0')
+
+    click.echo('Run: {}'.format(run_number))
+    click.echo('Outfile: {}'.format(outfile))
+    for setting, value in cfg['MuonGun_settings'].items():
+        click.echo('{}:'.format(setting), value)
+
+    # crate random services
+    if 'random_service_use_gslrng' not in cfg:
+        cfg['random_service_use_gslrng'] = False
+    random_services, _ = create_random_services(
+        dataset_number=cfg['dataset_number'],
+        run_number=cfg['run_number'],
+        seed=cfg['seed'],
+        n_services=2,
+        use_gslrng=cfg['random_service_use_gslrng'])
+
+    # --------------------------------------
+    # Build IceTray
+    # --------------------------------------
+    tray = I3Tray()
+
+    # add random generator to tray context
+    tray.context['I3RandomService'] = random_services[0]
+
+    tray.AddModule('I3InfiniteSource', 'source',
+                   # Prefix=gcdfile,
+                   Stream=icetray.I3Frame.DAQ)
+
+    if 'oversampling_factor' not in cfg:
+        cfg['oversampling_factor'] = None
+    if 'oversample_after_proposal' in cfg and \
+            cfg['oversample_after_proposal']:
+        oversampling_factor_injection = None
+        oversampling_factor_photon = cfg['oversampling_factor']
+    else:
+        oversampling_factor_injection = cfg['oversampling_factor']
+        oversampling_factor_photon = None
+
+    tray.AddSegment(
+        segments.GenerateCosmicRayMuons,
+        'GenerateCosmicRayMuons',
+        num_events=cfg['n_events_per_run'],
+        **cfg['GenerateCosmicRayMuonsSettings']
+    )
+
+    # add filter to bias simulation based on track geometry
+    tray.AddModule(
+        MuonGeometryFilter,
+        'MuonGeometryFilter',
+        **cfg['MuonGeometryFilterSettings']
+    )
+
+    tray.AddModule(DAQFrameMultiplier, 'DAQFrameMultiplier',
+                   oversampling_factor=oversampling_factor_injection)
+
+    # propagate muons if config exists in config
+    # Note: Snowstorm may perform muon propagation internally
+    if 'muon_propagation_config' in cfg:
+        tray.AddSegment(segments.PropagateMuons,
+                        'propagate_muons',
+                        RandomService=random_services[1],
+                        **cfg['muon_propagation_config'])
+    else:
+        # In this case we are not propagating the I3MCTree yet, but
+        # are letting this be done by snowstorm propagation
+        # We need to add a key named 'I3MCTree', since snowstorm expects this
+        # It will propagate the particles for us.
+        tray.AddModule(
+            'Rename', 'DummyMCTreeRenaming'
+            keys=['I3MCTree_preMuonProp', 'I3MCTree'],
+        )
+
+    # add filter to bias simulation based on muon loss profile
+    tray.AddModule(
+        MuonLossProfileFilter,
+        'MuonLossProfileFilter',
+        **cfg['MuonLossProfileFilterSettings']
+    )
+
+    tray.AddModule(DAQFrameMultiplier, 'DAQFrameMultiplier',
+                   oversampling_factor=oversampling_factor_photon)
+
+    # --------------------------------------
+    # Distance Splits
+    # --------------------------------------
+    if cfg['distance_splits'] is not None:
+        click.echo('SplittingDistance: {}'.format(
+            cfg['distance_splits']))
+        distance_splits = np.atleast_1d(cfg['distance_splits'])
+        dom_limits = np.atleast_1d(cfg['threshold_doms'])
+        if len(dom_limits) == 1:
+            dom_limits = np.ones_like(distance_splits) * cfg['threshold_doms']
+        oversize_factors = np.atleast_1d(cfg['oversize_factors'])
+        order = np.argsort(distance_splits)
+
+        distance_splits = distance_splits[order]
+        dom_limits = dom_limits[order]
+        oversize_factors = oversize_factors[order]
+
+        stream_objects = generate_stream_object(distance_splits,
+                                                dom_limits,
+                                                oversize_factors)
+        tray.AddModule(OversizeSplitterNSplits,
+                       "OversizeSplitterNSplits",
+                       thresholds=distance_splits,
+                       thresholds_doms=dom_limits,
+                       oversize_factors=oversize_factors)
+        for stream_i in stream_objects:
+            outfile_i = stream_i.transform_filepath(outfile)
+            tray.AddModule("I3Writer",
+                           "writer_{}".format(stream_i.stream_name),
+                           Filename=outfile_i,
+                           Streams=[icetray.I3Frame.DAQ,
+                                    icetray.I3Frame.Physics,
+                                    icetray.I3Frame.Stream('S'),
+                                    icetray.I3Frame.Stream('M')],
+                           If=stream_i)
+            click.echo('Output ({}): {}'.format(stream_i.stream_name,
+                                                outfile_i))
+    else:
+        click.echo('Output: {}'.format(outfile))
+        tray.AddModule("I3Writer", "writer",
+                       Filename=outfile,
+                       Streams=[icetray.I3Frame.DAQ,
+                                icetray.I3Frame.Physics,
+                                icetray.I3Frame.Stream('S'),
+                                icetray.I3Frame.Stream('M')])
+    # --------------------------------------
+
+    click.echo('Scratch: {}'.format(scratch))
+    tray.AddModule("TrashCan", "the can")
+    tray.Execute()
+    tray.Finish()
+
+
+if __name__ == '__main__':
+    main()
