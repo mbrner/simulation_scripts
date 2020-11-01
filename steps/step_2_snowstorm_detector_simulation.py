5c91558d2d5ac8b427e69ee4baf74a2516942036
mhuen <mhuen@users.noreply.github.com>
2020-11-01 12:25:27 +0100

    Workaround for segfault in detector sim --> revert back to combo_V01-00-00-RC0

--- steps/step_2_snowstorm_detector_simulation.py
+++ steps/step_2_snowstorm_detector_simulation.py
@@ -1,5 +1,7 @@
 #!/bin/sh /cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/icetray-start
-#METAPROJECT /mnt/lfs7/user/mhuennefeld/software/icecube/py3-v4.1.0/combo_V01-00-00/build
+#METAPROJECT /mnt/lfs7/user/mhuennefeld/software/icecube/py3-v4.1.0/combo_V01-00-00-RC0/build
+#--METAPROJECT /mnt/lfs7/user/mhuennefeld/software/icecube/py3-v4.1.0/combo_V01-00-00/build
+#--METAPROJECT combo/V01-00-00 # <-- Causes segfaults, therefore use RC0
 import os
 
 import click
